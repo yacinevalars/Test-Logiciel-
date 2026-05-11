804a95f97be3820217d406c1393ad44c6f8498f6
Yacine AIT HAMI <yacine@valars.com>
2026-05-11 10:17:24 +0200

    update files

--- src/test/java/com/example/demo/service/StatistiqueImplTests.java
+++ src/test/java/com/example/demo/service/StatistiqueImplTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class StatistiqueTests {
+public class StatistiqueImplTests {
 
     @Autowired
     StatistiqueImpl statistiqueImpl;
